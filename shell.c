899b3541da319c81dd104f674ac13066b8a372c7
Zachery Gentry <zacherygentry@gmail.com>
2018-09-16 16:09:08 -0500

    scrap printing from another function, keep it in same

--- shell.c
+++ shell.c
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-//
-// Copyright (c) 2016, 2017 Trevor Bakker
-//
+// 
+// Copyright (c) 2016, 2017 Trevor Bakker 
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,51 +30,42 @@
 #include <string.h>
 #include <signal.h>
 
-#define WHITESPACE " \t\n" // We want to split our command line up into tokens \
-                           // so we need to define what delimits our tokens.   \
-                           // In this case  white space                        \
-                           // will separate the tokens on our command line
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
 
-#define MAX_COMMAND_SIZE 255 // The maximum command-line size
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 5 // Mav shell only supports five arguments
-
-char *getInput(char *);
+#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports five arguments
 
 int main()
 {
-    char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
 
-    while (1)
-    {
-        getInput(cmd_str);
-    }
-    return 0;
-}
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
 
-char *getInput(char * cmd_str)
-{
+  while( 1 )
+  {
     // Print out the msh prompt
-    printf("msh> ");
+    printf ("msh> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
     // This while command will wait here until the user
     // inputs something since fgets returns NULL when there
     // is no input
-    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin))
-        ;
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
 
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
-    int token_count = 0;
-
+    int   token_count = 0;                                 
+                                                           
     // Pointer to point to the token
     // parsed by strsep
-    char *arg_ptr;
-
-    char *working_str = strdup(cmd_str);
+    char *arg_ptr;                                         
+                                                           
+    char *working_str  = strdup( cmd_str );                
 
     // we are going to move the working_str pointer so
     // keep track of its original value so we can deallocate
@@ -82,26 +73,28 @@ char *getInput(char * cmd_str)
     char *working_root = working_str;
 
     // Tokenize the input stringswith whitespace used as the delimiter
-    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) &&
-           (token_count < MAX_NUM_ARGUMENTS))
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+              (token_count<MAX_NUM_ARGUMENTS))
     {
-        token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
-        if (strlen(token[token_count]) == 0)
-        {
-            token[token_count] = NULL;
-        }
+      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
         token_count++;
     }
-    
+
+    // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your shell functionality
 
-    int token_index = 0;
-    for (token_index = 0; token_index < token_count; token_index++)
+    int token_index  = 0;
+    for( token_index = 0; token_index < token_count; token_index ++ ) 
     {
-        printf("token[%d] = %s\n", token_index, token[token_index]);
+      printf("token[%d] = %s\n", token_index, token[token_index] );  
     }
 
-    free(working_root);
+    free( working_root );
 
-    return token;
+  }
+  return 0;
 }
