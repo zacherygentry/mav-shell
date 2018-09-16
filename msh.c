a19f0e925387350e9812129f2d63a432e4423e70
Zachery Gentry <zacherygentry@gmail.com>
2018-09-16 15:36:55 -0500

    moved input to own method

--- msh.c
+++ msh.c
@@ -39,68 +39,70 @@
 
 #define MAX_NUM_ARGUMENTS 5 // Mav shell only supports five arguments
 
-char *getInput();
+char *getInput(char *);
 
 int main()
-{
-    getInput();
-    return 0;
-}
-
-char *getInput()
 {
     char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
 
     while (1)
     {
-        // Print out the msh prompt
-        printf("msh> ");
-
-        // Read the command from the commandline.  The
-        // maximum command that will be read is MAX_COMMAND_SIZE
-        // This while command will wait here until the user
-        // inputs something since fgets returns NULL when there
-        // is no input
-        while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin))
-            ;
-
-        /* Parse input */
-        char *token[MAX_NUM_ARGUMENTS];
-
-        int token_count = 0;
-
-        // Pointer to point to the token
-        // parsed by strsep
-        char *arg_ptr;
-
-        char *working_str = strdup(cmd_str);
-
-        // we are going to move the working_str pointer so
-        // keep track of its original value so we can deallocate
-        // the correct amount at the end
-        char *working_root = working_str;
-
-        // Tokenize the input stringswith whitespace used as the delimiter
-        while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) &&
-               (token_count < MAX_NUM_ARGUMENTS))
-        {
-            token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
-            if (strlen(token[token_count]) == 0)
-            {
-                token[token_count] = NULL;
-            }
-            token_count++;
-        }
-
-        // Now print the tokenized input as a debug check
-        // \TODO Remove this code and replace with your shell functionality
-
-        int token_index = 0;
-        for (token_index = 0; token_index < token_count; token_index++)
-        {
-            printf("token[%d] = %s\n", token_index, token[token_index]);
-        }
-
-        free(working_root);
+        char *input = getInput(cmd_str);
     }
+    return 0;
+}
+
+char *getInput(char * cmd_str)
+{
+    // Print out the msh prompt
+    printf("msh> ");
+
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin))
+        ;
+
+    /* Parse input */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    int token_count = 0;
+
+    // Pointer to point to the token
+    // parsed by strsep
+    char *arg_ptr;
+
+    char *working_str = strdup(cmd_str);
+
+    // we are going to move the working_str pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *working_root = working_str;
+
+    // Tokenize the input stringswith whitespace used as the delimiter
+    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) &&
+           (token_count < MAX_NUM_ARGUMENTS))
+    {
+        token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+        if (strlen(token[token_count]) == 0)
+        {
+            token[token_count] = NULL;
+        }
+        token_count++;
+    }
+
+    // Now print the tokenized input as a debug check
+    // \TODO Remove this code and replace with your shell functionality
+
+    int token_index = 0;
+    for (token_index = 0; token_index < token_count; token_index++)
+    {
+        printf("token[%d] = %s\n", token_index, token[token_index]);
+    }
+
+    free(working_root);
+
+    return token;
 }
